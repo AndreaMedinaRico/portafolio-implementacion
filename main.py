ee7c5fdba547070e2880a809bcebcb3acfaaedaa
AndreaMedinaRico <a01705541@tec.mx>
2025-08-27 20:34:49 -0600

    refactor: transformation class

--- main.py
+++ main.py
@@ -5,34 +5,27 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from transformation import Transformation
+
 pd.set_option('display.max_columns', None)  # Muestra todas las columnas
-pd.set_option('display.width', 200)         # Ajusta el ancho (puedes cambiar el número)
+pd.set_option('display.width', 200)         # Ajusta el ancho
 
 penguins = pd.read_csv("penguins_size.csv")
 
 # ------ TRANSFORMACIÓN --------
+trans = Transformation(penguins)
 
-# Eliminar valores vacíos
-penguins = penguins.dropna()
+trans.drop_na()
+trans.cat_to_num('sex', 'MALE', 'FEMALE')
+trans.one_hot_encoding('species')
+trans.one_hot_encoding('island')
 
-# Tranformar categóricos a numéricos
-penguins['sex'] = penguins['sex'].map({'MALE': 1, 'FEMALE': 0})
+trans.change_units('body_mass_g', 1000)
+trans.change_units('flipper_length_mm', 10)
+trans.change_units('culmen_depth_mm', 10)
+trans.change_units('culmen_length_mm', 10)
 
-#   One-Hot encoding
-penguins = pd.get_dummies(penguins, columns=['species'], prefix = 'species')
-penguins = pd.get_dummies(penguins, columns=['island'], prefix = 'island')
-for col in penguins.columns:
-    if penguins[col].dtype == 'bool':
-        penguins[col] = penguins[col].astype(float)
-
-# Verificar unidades de medida
-penguins['body_mass_g'] = penguins['body_mass_g'] / 1000
-penguins['flipper_length_mm'] = penguins['flipper_length_mm'] / 10
-penguins['culmen_depth_mm'] = penguins['culmen_depth_mm'] / 10 
-penguins['culmen_length_mm'] = penguins['culmen_length_mm'] / 10 
-
-# Actualizar nombre de columnas
-penguins = penguins.rename(columns={
+trans.rename_columns({
     'body_mass_g': 'body_mass_kg',
     'flipper_length_mm': 'flipper_length_cm',
     'culmen_depth_mm': 'culmen_depth_cm',
@@ -40,6 +33,6 @@ penguins = penguins.rename(columns={
 })
 
 # Buscar anomalías
-print(penguins.info())
-print(penguins.describe())
-print(penguins.head())
+print(trans.data.info())
+print(trans.data.describe())
+print(trans.data.head())
---
+++ transformation.py
@@ -0,0 +1,35 @@
+'''
+Archivo: etl.py
+Descripción: Este archivo contiene las funciones para extraer, transformar
+    y limpiar el dataset 'penguins_size.csv'.
+Autora: Andrea Medina Rico
+'''
+import pandas as pd
+
+class Transformation:
+    def __init__(self, data):
+        self.data = data
+
+    
+    def drop_na(self):
+        self.data = self.data.dropna()
+    
+
+    def cat_to_num(self, column, x1, x2):
+        self.data[column] = self.data[column].map({x1: 1, x2: 0})
+
+
+    def one_hot_encoding(self, column):
+        self.data = pd.get_dummies(self.data, columns = [column], prefix = column)
+
+        for col in self.data.columns:
+            if self.data[col].dtype == 'bool':
+                self.data[col] = self.data[col].astype(float)
+
+
+    def change_units(self, column, const):
+        self.data[column] = self.data[column] / const
+
+
+    def rename_columns(self, columns_dict):
+        self.data.rename(columns = columns_dict, inplace = True)
