eb8011fd36e679585e6c2aa6bc5bfca7df0e287c
AndreaMedinaRico <a01705541@tec.mx>
2025-08-27 19:24:40 -0600

    feat: tranformacion de datos pingüinos

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 '''
 Autora: Andrea Medina Rico
 '''
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,46 +8,33 @@ import matplotlib.pyplot as plt
 pd.set_option('display.max_columns', None)  # Muestra todas las columnas
 pd.set_option('display.width', 200)         # Ajusta el ancho (puedes cambiar el número)
 
-# ---- TRANSFORMACIÓN ----- 
-songs = pd.read_csv("dataset.csv")
-print(songs.head())
-print(songs.tail())
+penguins = pd.read_csv("penguins_size.csv")
+print(penguins.head())
+print(penguins.tail())
 
-print(songs.info())
+# ------ TRANSFORMACIÓN --------
 
-# Mantener solo columnas relevantes
-songs.drop(columns = ['track_id', 'artists', 'album_name', 'mode', 'time_signature', 'explicit', 'track_genre'], inplace = True)
+# Eliminar valores vacíos
+penguins = penguins.dropna()
+print(penguins.info())
 
-# Unidades de medida y tipos de dato
-songs['duration_ms'] = songs['duration_ms'].apply(lambda x: x / 1000 / 60)
-songs.rename(columns={'duration_ms': 'duration_min'}, inplace = True) 
+# Tranformar a numéricos
+penguins['sex'] = penguins['sex'].map({'MALE': 1, 'FEMALE': 0})
+print(penguins.head())
 
-# Valores repetidos
-uniques = songs['track_name'].value_counts()
-songs = songs[songs['track_name'].isin(uniques[uniques == 1].index)]
+# Verificar unidades de medida
+penguins['body_mass_g'] = penguins['body_mass_g'] / 1000  # Convertir a kg
+penguins['flipper_length_mm'] = penguins['flipper_length_mm'] / 10  # Convertir a cm
+penguins['culmen_depth_mm'] = penguins['culmen_depth_mm'] / 10  # Convertir a cm
+penguins['culmen_length_mm'] = penguins['culmen_length_mm'] / 10  # Convertir a cm
 
-songs.drop('track_name', axis = 1, inplace = True)
+# Actualizar nombre de columnas
+penguins = penguins.rename(columns={
+    'body_mass_g': 'body_mass_kg',
+    'flipper_length_mm': 'flipper_length_cm',
+    'culmen_depth_mm': 'culmen_depth_cm',
+    'culmen_length_mm': 'culmen_length_cm'
+})
 
-# Valores atípicos
-songs = songs[songs['duration_min'] < 12]
-
-print(songs.head())
-print("\n\nDescripción:\n\n\n")
-print(songs.describe())
-
-'''
-# Selecciona solo las columnas numéricas, excluyendo 'song_popularity'
-muestra = songs.sample(n=200, random_state=42)  # Toma 200 filas aleatorias
-
-columnas_numericas = muestra.select_dtypes(include='number').columns.drop('popularity')
-
-for col in columnas_numericas:
-    plt.figure()
-    plt.scatter(muestra[col], muestra['popularity'], alpha=0.5)
-    plt.xlabel(col)
-    plt.ylabel('popularity')
-    plt.title(f'{col} vs popularity (muestra aleatoria)')
-    plt.show()
-'''
-
-print(songs.corr())
+# Buscar anomalías
+print(penguins.describe())
