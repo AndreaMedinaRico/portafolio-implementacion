6363a1fb3dbfddc2e2a7b0594adfe44816d2c4b0
AndreaMedinaRico <a01705541@tec.mx>
2025-08-27 20:13:13 -0600

    feat: one hot encoding

--- main.py
+++ main.py
@@ -9,24 +9,27 @@ pd.set_option('display.max_columns', None)  # Muestra todas las columnas
 pd.set_option('display.width', 200)         # Ajusta el ancho (puedes cambiar el número)
 
 penguins = pd.read_csv("penguins_size.csv")
-print(penguins.head())
-print(penguins.tail())
 
 # ------ TRANSFORMACIÓN --------
 
 # Eliminar valores vacíos
 penguins = penguins.dropna()
-print(penguins.info())
 
-# Tranformar a numéricos
+# Tranformar categóricos a numéricos
 penguins['sex'] = penguins['sex'].map({'MALE': 1, 'FEMALE': 0})
-print(penguins.head())
+
+#   One-Hot encoding
+penguins = pd.get_dummies(penguins, columns=['species'], prefix = 'species')
+penguins = pd.get_dummies(penguins, columns=['island'], prefix = 'island')
+for col in penguins.columns:
+    if penguins[col].dtype == 'bool':
+        penguins[col] = penguins[col].astype(float)
 
 # Verificar unidades de medida
-penguins['body_mass_g'] = penguins['body_mass_g'] / 1000  # Convertir a kg
-penguins['flipper_length_mm'] = penguins['flipper_length_mm'] / 10  # Convertir a cm
-penguins['culmen_depth_mm'] = penguins['culmen_depth_mm'] / 10  # Convertir a cm
-penguins['culmen_length_mm'] = penguins['culmen_length_mm'] / 10  # Convertir a cm
+penguins['body_mass_g'] = penguins['body_mass_g'] / 1000
+penguins['flipper_length_mm'] = penguins['flipper_length_mm'] / 10
+penguins['culmen_depth_mm'] = penguins['culmen_depth_mm'] / 10 
+penguins['culmen_length_mm'] = penguins['culmen_length_mm'] / 10 
 
 # Actualizar nombre de columnas
 penguins = penguins.rename(columns={
@@ -37,4 +40,6 @@ penguins = penguins.rename(columns={
 })
 
 # Buscar anomalías
+print(penguins.info())
 print(penguins.describe())
+print(penguins.head())
