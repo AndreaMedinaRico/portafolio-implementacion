730b3984ab2752da162ba72cead70ecb8589dac9
AndreaMedinaRico <a01705541@tec.mx>
2025-08-28 19:43:27 -0600

    feat: archivo statistics y gráficas de datos

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Autora: Andrea Medina Rico
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from transformation import Transformation
 
@@ -38,3 +39,57 @@ trans.rename_columns({
 print(trans.data.info())
 print(trans.data.describe())
 print(trans.data.head())
+
+
+# ------ ESTADÍSTICA --------
+
+# Matriz de correlación
+corr_matrix = trans.data.corr()
+plt.figure(figsize=(10, 8))
+sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+plt.title('Matriz de correlación')
+plt.show()
+
+# Pairplot con seaborn
+'''
+sns.pairplot(trans.data)
+plt.title('Pairplot de las variables numéricas')
+plt.show()
+'''
+
+# Subplots de sctatterplot matplot
+plt.subplot(231)
+plt.scatter(trans.data['flipper_length_cm'], trans.data['body_mass_kg'])
+plt.title('Body Mass vs Flipper Length')
+plt.subplot(232)
+plt.scatter(trans.data['culmen_length_cm'], trans.data['body_mass_kg'])
+plt.title('Body Mass vs Culmen Length')
+plt.subplot(233)
+plt.scatter(trans.data['culmen_depth_cm'], trans.data['body_mass_kg'])
+plt.title('Body Mass vs Culmen Depth')
+plt.suptitle('Relación entre Body Mass y otras variables')
+plt.subplot(234)
+plt.scatter(trans.data['species_Gentoo'], trans.data['body_mass_kg'])
+plt.title('Body Mass vs Species (Gentoo)')
+plt.subplot(235)
+plt.scatter(trans.data['island_Biscoe'], trans.data['body_mass_kg'])
+plt.title('Body Mass vs Island (Biscoe)')
+plt.subplot(236)
+plt.scatter(trans.data['species_Adelie'], trans.data['body_mass_kg'])
+plt.title('Body Mass vs Species (Adelie)')
+plt.show()
+
+# Kdeplot
+plt.subplot(221)
+sns.kdeplot(trans.data['body_mass_kg'], fill = True)
+plt.title('Distribución de Body Mass')
+plt.subplot(222)
+sns.kdeplot(trans.data['flipper_length_cm'], fill = True)
+plt.title('Distribución de Flipper Length')
+plt.subplot(223)
+sns.kdeplot(trans.data['culmen_length_cm'], fill = True)
+plt.title('Distribución de Culmen Length')
+plt.subplot(224)
+sns.kdeplot(trans.data['culmen_depth_cm'], fill = True)
+plt.title('Distribución de Culmen Depth')
+plt.show()
---
+++ statistic.py
@@ -0,0 +1,14 @@
+'''
+Archivo: statistic.py
+Descripción: Archivo para realizar un análisis estadístico
+    sobre las variables del dataset 'penguins_size.csv'.
+Autora: Andrea Medina Rico
+'''
+
+class Statistic:
+    def __init__(self, data):
+        self.data = data
+
+
+    def hypothesis_testing(self):
+        return
